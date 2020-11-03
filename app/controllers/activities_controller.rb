ae7b3bb1d098645ae3a664dd33fc96102ff1c3e7
StevenWuTG <stevenwu91@icloud.com>
2020-11-03 11:38:09 -0500

    done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,15 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
   end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :difficulty)
+  end
+  
 end
